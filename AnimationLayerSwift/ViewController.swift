b1240eebac599c299d4e2d4d3282fb51c1fa8b71
SPIREJ <849703986@qq.com>
2019-03-21 11:31:02 +0800

    layer animation start

---
+++ AnimationLayerSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationLayerSwift
+//
+//  Created by SPIREJ on 2019/3/21.
+//  Copyright © 2019年 xiangshang360. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
